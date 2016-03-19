1430ef913f3ec6621e4ba91474cc9058a0a4dccf
Sergey Philimonov <saint.deos@gmail.com>
2016-03-19 21:18:23 +0200

    Add Vote. Cleam tmp files

--- petitions/app/controllers/votes_controller.rb
+++ petitions/app/controllers/votes_controller.rb
@@ -1,2 +1,13 @@
 class VotesController < ApplicationController
+  before_filter :authorize
+
+  def create
+    vote = current_user.votes.create(permitted_params)
+    flash["alert-success"] = 'Спасибо! Ваш голос учтен'
+    redirect_to vote.petition
+  end
+
+  def permitted_params
+    params.permit(:petition_id)
+  end
 end
